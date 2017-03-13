caa2c53ec51a33eebbb728ee8f7f4de09e993e2d
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-03-13 16:25:41 -0700

    working on api call

--- controllers/results.js
+++ controllers/results.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.get('/recipes', function(req, res) {
-  var url = 'https://api.edamam.com/search' + queryString + '&app_id=c8ceed5f&app_key=bbfa5375222109bd6452b480ab860eaa&from=0&to=' + queryParams +'';
-  var url = 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=' + key;
+router.get('/weather', function(req, res) {
+  var url = 'http://api.openweathermap.org/data/2.5/forecast/city?id=5809844&APPID=' + key;
     request(url, function(error, response, body) {
       res.send(body);
     });
