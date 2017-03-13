cd04954ef3e39902ac21277c733e2a2b37c330db
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-03-13 15:32:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var request = require('request');
+var app = express();
+
+//Decode POST data in JSON and URL encoded formats
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//Setup Path directory (static)
+app.use(express.static(path.join(__dirname, 'static')));
+//Observe server activity
+app.use(require('morgan')('dev'));
+
+app.use('/api/results', require('./controllers/results'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'static/index.html'));
+});
+
+var server = app.listen((process.env.PORT || 3000), function(){
+  console.log('listening on 3000');
+});
+
+module.exports = server;
